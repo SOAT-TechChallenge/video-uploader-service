a1ebfc199c198e3a0750914348d43720e32136a5
leynerbueno <leyner_henrique@hotmail.com.br>
2026-02-01 01:08:32 -0300

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -32,22 +32,22 @@ variable "aws_access_key_id" {
   description = "AWS Access Key ID"
   type        = string
   sensitive   = true
-  default     = "ASIAUJBCHKTQ3T2CX5Q6"
+  default     = ""
 }
 
 variable "aws_secret_access_key" {
   description = "AWS Secret Access Key"
   type        = string
   sensitive   = true
-  default     = "6JtKu6harqvxeajOGXEEClQ04KOptfD4XShYKO43"
+  default     = ""
 }
 
 variable "aws_session_token" {
   description = "AWS Session Token"
   type        = string
   sensitive   = true
-  default     = "IQoJb3JpZ2luX2VjEPj//////////wEaCXVzLXdlc3QtMiJHMEUCIEPys8syD4E8qtPL7omgx0nSXRvCZV+RoK3Ek3SIpv2sAiEA6Z0aeF2WOvxVkiP9KRLLp3KCOYByaLFGW/zdidW9OpsqwQIIwf//////////ARABGgwyOTQyNzcwNDM0MjUiDKVH11mfZN5N8TU3DCqVAsWrxmVZJB12I5QHAiVm5RACSVcxqIjzSg4vH165y+fDr0q4I9MvdCJ6C0tHMOCQS5WFG6f6bGN48z4QgemKrSHsHYngZXAKBQHG9JHoTZ6Rkm4C8+HVr00Xx+MwAVyBZUD4jqlBCN4dkLyl2jvRqzglSOA6Pn+UbDULYmks/0BNhFyW0aPXsUT4IfbjThHzi4zIhMAYJoBEaKYgZrFXNOU1fi6ZJi8JujLVEYe0uCCwv+jTaXrQkYjN9v9ztmxzjIXghtICdA7XsxgwYh4+n+nGxcrz1WM4vDABkY3oPS33IpmGXhWP65n0+Tz66GzW+gdwj8qKheB17N1YXGDA96DHT9VPtosNzt18qVFqjP3DMg4fV/gw3Kj6ywY6nQHpPsig2ew3MGVsLD/VpXbmvmTIWKQR6i1GR+EHG7F20MZbjSIrinMFCBHJ71phs5i12ecajC92SE+0mosfbbkZ0qnEke/9u5KyWPom4TZa/3ZpIgjlXbv/UhdNliAu2dwz2bdf+LiwGynMzaCOXsLUulcLqpw0sHfGAYwLdzPIZi5fWibl08yqPop6XhUiHj5fctKCxxA8YdZ0JY4m"
-  }
+  default     = ""
+    }
 
 variable "aws_s3_bucket" {
   description = "Nome do bucket S3 para vídeos"
